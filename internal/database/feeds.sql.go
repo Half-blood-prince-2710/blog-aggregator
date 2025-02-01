6733fbacaef2c664a4564295d4d379e044493181
Half-blood-prince-2710 <guptadmanish2710@gmail.com>
2025-02-01 22:40:29 +0530

    generating go code for query

--- internal/database/feeds.sql.go
+++ internal/database/feeds.sql.go
@@ -37,6 +37,17 @@ func (q *Queries) CreateFeed(ctx context.Context, arg CreateFeedParams) (Feed, e
 	return i, err
 }
 
+const getFeedByUrl = `-- name: GetFeedByUrl :one
+SELECT id FROM feeds WHERE url = $1
+`
+
+func (q *Queries) GetFeedByUrl(ctx context.Context, url string) (uuid.UUID, error) {
+	row := q.db.QueryRowContext(ctx, getFeedByUrl, url)
+	var id uuid.UUID
+	err := row.Scan(&id)
+	return id, err
+}
+
 const getFeeds = `-- name: GetFeeds :many
 SELECT name,url,user_id FROM feeds
 `
