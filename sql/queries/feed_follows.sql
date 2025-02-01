3611848e7e042109e549bb046312c37022b3563f
Half-blood-prince-2710 <guptadmanish2710@gmail.com>
2025-02-01 22:29:58 +0530

    generating go code for sql query

--- sql/queries/feed_follows.sql
+++ sql/queries/feed_follows.sql
@@ -1,6 +1,6 @@
 -- name: CreateFeedFollow :one
 With inserted_feed_follow AS (
-    INSERT INTO feed_follow(user_id,feed_id) VALUES($1,$2) RETURNING *
+    INSERT INTO feed_follows(user_id,feed_id) VALUES($1,$2) RETURNING *
 )
 
 SELECT inserted_feed_follow.*, feeds.name AS feed_name, users.name AS user_name FROM inserted_feed_follow
