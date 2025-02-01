49e85168be1423aa062bb997a43008b8a6340716
Half-blood-prince-2710 <guptadmanish2710@gmail.com>
2025-02-01 17:51:12 +0530

    registered handleusers command

--- main.go
+++ main.go
@@ -40,7 +40,8 @@ func main(){
 	cmds.register("login",handlerLogin)
 	cmds.register("register",handlerRegister)
 	cmds.register("reset",handlerReset)
-
+	cmds.register("users",handlerUsers)
+	
 	//checking if arguments are less than 2
 	fmt.Print(os.Args,"\n")
 	if len(os.Args)<2 {
