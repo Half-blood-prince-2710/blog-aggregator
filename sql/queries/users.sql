24557a60cc50b484efb4863d662392bb12b1b153
Half-blood-prince-2710 <guptadmanish2710@gmail.com>
2025-02-01 17:32:18 +0530

    updated querirs with getusers query

--- sql/queries/users.sql
+++ sql/queries/users.sql
@@ -13,3 +13,6 @@ SELECT id,created_at,updated_at,name FROM users WHERE name = $1;
 
 -- name: DeleteAllUser :exec 
 DELETE FROM users;
+
+-- name: GetUsers :many
+SELECT id,created_at,updated_at,name FROM users;
