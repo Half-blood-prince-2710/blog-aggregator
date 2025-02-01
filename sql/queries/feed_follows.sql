c03628f36d559cba233e6d117b628259158b7d6b
Half-blood-prince-2710 <guptadmanish2710@gmail.com>
2025-02-01 22:28:20 +0530

    created inserted feed_follow query

---
+++ sql/queries/feed_follows.sql
@@ -0,0 +1,8 @@
+-- name: CreateFeedFollow :one
+With inserted_feed_follow AS (
+    INSERT INTO feed_follow(user_id,feed_id) VALUES($1,$2) RETURNING *
+)
+
+SELECT inserted_feed_follow.*, feeds.name AS feed_name, users.name AS user_name FROM inserted_feed_follow
+INNER JOIN users ON users.id = inserted_feed_follow.user_id
+INNER JOIN feeds ON feeds.id = inserted_feed_follow.feed_id;
