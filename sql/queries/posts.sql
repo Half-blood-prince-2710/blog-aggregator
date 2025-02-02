85bf0f4b15e15a300247f3f04e5f6cd5d07b35e0
Half-blood-prince-2710 <guptadmanish2710@gmail.com>
2025-02-02 16:43:14 +0530

    writing queries

--- sql/queries/posts.sql
+++ sql/queries/posts.sql
@@ -2,3 +2,11 @@
 INSERT INTO posts(title,url,description,published_at,feed_id)
 VALUES ($1,$2,$3,$4,$5)
 RETURNING *;
+
+-- name: GetPostsForUser :many
+SELECT posts.*
+FROM posts
+JOIN feed_follows ON posts.feed_id = feed_follows.feed_id
+WHERE feed_follows.user_id = $1
+ORDER BY posts.created_at DESC
+LIMIT $2;
